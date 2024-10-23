bf13bd430bfcc883ee61ab8df2661fe16b9d21fc
OctavioAquilino <tayiaquilino12345@gmail.com>
2024-10-23 16:04:21 -0300

    .

--- fir.py
+++ fir.py
@@ -3,13 +3,17 @@ import numpy as np
 
 # Parámetros del filtro
 num_taps = 101  # Número de coeficientes (ajústalo según tus necesidades)
-fs = 48000  # Frecuencia de muestreo (ajústala según tus datos)
-cutoff_freq = [1500, 2300]  # Frecuencias de corte (en Hz) para un filtro pasa-bandas
+fs = 22050  # Frecuencia de muestreo (ajústala según tus datos)
+cutoff_freq = [1100, 1300]  # Frecuencias de corte (en Hz) para un filtro rechaza-bandas
 
-# Diseño del filtro FIR pasa-bandas
+# Diseño del filtro FIR rechaza-bandas
 fir_coef = firwin(num_taps, cutoff_freq, pass_zero='bandstop', fs=fs)
 
+# Convertir los coeficientes a un formato de cadena separado por comas
+coef_str = ', '.join([f'{coef:.18e}' for coef in fir_coef])
+
 # Guardar los coeficientes en un archivo de texto
-np.savetxt('fir_taps.txt', fir_coef, fmt='%.18e')
+with open('fir_taps.txt', 'w') as f:
+    f.write(coef_str)
 
 print(fir_coef)
