60ac8d6ed172ae47aefe5bb3756f3d3ebb9cbe2b
OctavioAquilino <tayiaquilino12345@gmail.com>
2024-12-08 13:28:49 -0300

    Pruebas

--- PARCIAL1_Rojo_Aquilino/ejercicio 1/FIR utilizado_GNU/filter.py
+++ PARCIAL1_Rojo_Aquilino/ejercicio 1/FIR utilizado_GNU/filter.py
@@ -2,18 +2,15 @@ import numpy as np
 from scipy.signal import firwin, freqz
 import matplotlib.pyplot as plt
 
-
-# Parámetros del filtro
-fs = 22050  # Frecuencia de sampleo
-fcd = 1100 # Frecuencia de corte (1.2 kHz)
+fs = 22050  
+fcd = 1100 
 fcu = 1300
-num_taps = 121  # Número de coeficientes del filtro FIR
+num_taps = 121  
 
-# Diseño del filtro FIR
 fir_filter = firwin(num_taps, cutoff=[fcd,fcu], fs=fs,pass_zero=False)
 print(fir_filter)
 
-with open("fir_taps_1200hz.txt", "w") as f:
+with open("fir_taps_1200hz_test.txt", "w") as f:
     f.write(','.join(map(str, fir_filter)))
 
 w, h = freqz(fir_filter, fs=fs)
